136c60b52a3a8af5085a5cf754c22102db083d99
PrabhatTheCoder <Prabhat.patel7447@yahoo.com>
2025-05-26 15:36:48 +0530

    Updated Code

--- food_waste_management/settings.py
+++ food_waste_management/settings.py
@@ -29,6 +29,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 
 # Application definition
 
